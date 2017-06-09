c4d264a7d45de69261df0d854fe9827393acc0e5
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2017-06-09 18:01:52 +0200

    feat: add first 3 files HTML CSS JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.onload = function(){
+	document.getElementById("lets_go").innerHTML = "Calculator!";
+	//document.getElementById("change_me").onclick = function(){
+	document.getElementById("change_me").toggle = function(){
+	document.getElementById("lets_go").innerHTML = "Calculator Progress!";
+	};
+
+};
+/*
+
+$(document).ready(function(){
+    $("#lets_go").text("Lemme Go!");
+});
+
+*/
+
