951d66c2cbc8454290323ca21c9925c67368f6f7
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2017-06-12 23:31:59 +0200

    feat: toggle button Use JavaScript to make a button toggle between different values for the innerHTML of a div. Don't use jQuery

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-
+//MAKE IT TOGGLE!!!
 window.onload = function(){
-	document.getElementById("lets_go").innerHTML = "Calculator!";
-	//document.getElementById("change_me").onclick = function(){
-	document.getElementById("change_me").toggle = function(){
-	document.getElementById("lets_go").innerHTML = "Calculator Progress!";
+	document.getElementById("lets_go").innerHTML = "Calculatorey!";
+	document.getElementById("change_me").onclick = function(){
+	//document.getElementById("change_me").toggle = function(){
+		if(document.getElementById("lets_go").innerHTML != "Calculator Progress!"){
+			document.getElementById("lets_go").innerHTML = "Calculator Progress!";
+		}
+		else{
+			document.getElementById("lets_go").innerHTML = "Calculatorey!";
+		}
 	};
 
 };
-/*
-
-$(document).ready(function(){
-    $("#lets_go").text("Lemme Go!");
-});
-
-*/
-
